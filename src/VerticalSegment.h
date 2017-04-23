984adc767e91bbb114c7a2840f3edf500e1bbfcc
Charlie Hendricks <themourningdawn@gmail.com>
2017-04-22 19:51:57 -0500

    Created stub of VerticalSegment that inherits from a Segment, based on the idea that a segment should be created with orientation in mind. It will mainly wrap Segment methods using Top And Bottom as boundry words. The orientation will be difined at instantiation time base off of which way data is flowing on the strip in relation to the ground.

---
+++ src/VerticalSegment.h
@@ -0,0 +1,31 @@
+#ifndef VERTICALSEGMENT_H
+#define VERTICALSEGMENT_H
+
+#include "application.h"
+#include "Segment.h"
+
+class VerticalSegment : public Segment
+{
+  private:
+    enum orientation : char;
+  public:
+    VerticalSegment();
+    VerticalSegment(CRGB *segment, int firstLEDIndex, int lastLEDIndex, orientation verticalOrientation);
+
+    int getTopLEDIndex();
+    int getBottomLEDIndex();
+
+    void setTopLEDColor(CRGB color);
+    void setBottomLEDColor(CRGB color);
+
+    void getTopLEDColor();
+    void getBottomLEDColor();
+
+    void shiftTowardTop();
+    void shiftTowardBottom();
+
+    void fillFromTop(CRGB color, int numberOfLEDToFIll);
+    void fillFromBottom(CRGB color, int numberOfLEDToFIll);
+};
+
+#endif
